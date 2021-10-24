cb2082ee998dcd858b41ca0fa6e93be721f7be9b
M L <magiconion_M@hotmail.com>
2021-10-24 10:44:07 +0100

    updates

--- posts/Azure-Upload-File-PoSH-Function-App/code/setup_environment.ps1
+++ posts/Azure-Upload-File-PoSH-Function-App/code/setup_environment.ps1
@@ -1,33 +1,4 @@
----
-title: Upload Files to Azure Storage using a PowerShell Function App
-published: false
-description: Azure - Function App file uploader
-tags: 'tutorial, powershell, productivity, azure'
-cover_image: 'https://raw.githubusercontent.com/Pwd9000-ML/blog-devto/main/posts/Azure-Upload-File-PoSH-Function-App/assets/main-cover.png'
-canonical_url: null
-id: 874213
----
-
-## Overview
-
-With Hacktober 2021 coming to an end soon, I thought I would share with you a little experiment I did using an Azure serverless Function App with Powershell as the code base. The idea was to create an easy to use, reusable `File Uploader API` that would allow someone to upload a file to an Azure Storage Account blob container by posting a HTTP request.  
-
-The HTTP request would be a JSON body and only requires the file name and the file Content/data in a serialized Base64 string. The PowerShell Function App would then deserialize the base64 string into a temporary file, rename and copy the file as a blob into a storage account container called `fileuploads`.
-
-## Set everything up automatically
-
-To stage and setup the entire environment for my API automatically I wrote a PowerShell script using AZ CLI, that would build and configure all the things I would need to start work on my function. There was one manual step however I will cover a bit later on. But for now you can find the script I used on my [github code](https://github.com/Pwd9000-ML/blog-devto/tree/main/posts/Azure-Upload-File-PoSH-Function-App/code) page called `setup_environment.ps1`.  
-
-First we will log into Azure by running:
-
-```powershell
-az login
-```
-
-After logging into Azure and selecting the subscription comes the script that will set everything up:
-
-```powershell
-# Setup Variables.
+﻿# Setup Variables.
 $randomInt = Get-Random -Maximum 9999
 $subscriptionId = (get-azcontext).Subscription.Id
 $resourceGroupName = "Function-App-Storage2"
@@ -111,12 +82,3 @@ $functionMI = $(az resource list --name $functionAppName --query [*].identity.pr
         --assignee "$_" `
         --scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$secureStore/blobServices/default/containers/$secureContainer"
     }
-```
-
-I hope you have enjoyed this post and have learned something new. You can also find the code samples used in this blog post on my [Github](https://github.com/Pwd9000-ML/blog-devto/tree/main/posts/Azure-Upload-File-PoSH-Function-App/code) page. :heart:
-
-### _Author_
-
-Like, share, follow me on: :octopus: [GitHub](https://github.com/Pwd9000-ML) | :penguin: [Twitter](https://twitter.com/pwd9000) | :space_invader: [LinkedIn](https://www.linkedin.com/in/marcel-l-61b0a96b/)
-
-{% user pwd9000 %}
